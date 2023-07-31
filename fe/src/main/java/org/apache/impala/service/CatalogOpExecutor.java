cd2366caf7b1d1b36419a88138437c80d2f9ed89
Andrew Sherman <asherman@cloudera.com>
2023-07-31 11:10:32 -0700

    Revert "TRY using commitIcebergTransaction"
    This reverts commit 58c3ba629dfed04f3c1caf46c361997242fc9d6f.

--- fe/src/main/java/org/apache/impala/service/CatalogOpExecutor.java
+++ fe/src/main/java/org/apache/impala/service/CatalogOpExecutor.java
@@ -6860,7 +6860,11 @@ public class CatalogOpExecutor {
           catalog_.addVersionsForInflightEvents(false, table, newCatalogVersion);
         }
 
-        commitIcebergTransaction(update, iceTxn);
+        if (update.isSetDebug_action()) {
+          String debugAction = update.getDebug_action();
+          DebugUtils.executeDebugAction(debugAction, DebugUtils.ICEBERG_COMMIT);
+        }
+        iceTxn.commitTransaction();
       }
 
       loadTableMetadata(table, newCatalogVersion, true, false, partsToLoadMetadata,
@@ -6883,14 +6887,6 @@ public class CatalogOpExecutor {
           DebugUtils.INSERT_FINISH_DELAY);
     }
     return response;
-  }
-
-  private void commitIcebergTransaction(TUpdateCatalogRequest update, org.apache.iceberg.Transaction iceTxn) {
-    if (update.isSetDebug_action()) {
-      String debugAction = update.getDebug_action();
-      DebugUtils.executeDebugAction(debugAction, DebugUtils.ICEBERG_COMMIT);
-    }
-    iceTxn.commitTransaction();
   }
 
   /**

51d0b6b862f8c1801bf466a310148543a8ccc866
Andrew Sherman <asherman@cloudera.com>
2024-02-22 18:44:30 -0800

    Move key_matched back to the method level
    Change-Id: If3326d969defc407ea9aa0e96a02a6d844f995b7

--- be/src/scheduling/admission-controller.cc
+++ be/src/scheduling/admission-controller.cc
@@ -1119,9 +1119,9 @@ bool AdmissionController::HasAvailableSlots(const ScheduleState& state,
 }
 
 bool AdmissionController::HasUserAndGroupPoolQuotas(const ScheduleState& state,
-    const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason,
-    bool& key_matched) {
+    const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason) {
   const string& user = state.request().query_ctx.session.delegated_user;
+  bool key_matched = false;
   if (!checkQuota(
           pool_cfg, pool_stats, state, user, quota_exceeded_reason, false, &key_matched)) {
     return false;
@@ -1146,9 +1146,9 @@ bool AdmissionController::HasUserAndGroupPoolQuotas(const ScheduleState& state,
 }
 
 bool AdmissionController::HasUserAndGroupRootQuotas(const ScheduleState& state,
-    const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason,
-    bool& key_matched) {
+    const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason) {
   const string& user = state.request().query_ctx.session.delegated_user;
+  bool key_matched = false;
   if (!checkQuota(
           pool_cfg, pool_stats, state, user, quota_exceeded_reason, false, &key_matched)) {
     return false;
@@ -1277,17 +1277,10 @@ bool AdmissionController::CanAdmitRequest(const ScheduleState& state,
           coordinator_resource_limited, not_admitted_details)) {
     return false;
   }
-  bool key_matched = false;
-  if (!HasUserAndGroupPoolQuotas(
-          state, pool_cfg, pool_stats, not_admitted_reason, key_matched)) {
+  if (!HasUserAndGroupPoolQuotas(state, pool_cfg, pool_stats, not_admitted_reason)) {
     return false;
   }
-  if (key_matched) {
-    // Matched a user level rule so no need for root level rules.
-    return true;
-  }
-  if (!HasUserAndGroupRootQuotas(
-          state, pool_cfg, pool_stats, not_admitted_reason, key_matched)) {
+  if (!HasUserAndGroupRootQuotas(state, pool_cfg, pool_stats, not_admitted_reason)) {
     return false;
   }
   return true;
--- be/src/scheduling/admission-controller.h
+++ be/src/scheduling/admission-controller.h
@@ -1108,13 +1108,11 @@ class AdmissionController {
   /// Returns true unless this query exceeds user or group quotas.
   /// Must hold admission_ctrl_lock_.
   static bool HasUserAndGroupPoolQuotas(const ScheduleState& state,
-      const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason,
-      bool& key_matched);
+      const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason);
 
   // FIXME describe
   static bool HasUserAndGroupRootQuotas(const ScheduleState& state,
-      const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason,
-      bool& key_matched);
+      const TPoolConfig& pool_cfg, PoolStats* pool_stats, string* quota_exceeded_reason);
 
   /// Updates the memory admitted and the num of queries running for each backend in
   /// 'state'. Also updates the stats of its associated resource pool. Used only when

7c934515b1b7ba18185de65da62a2f1fc9e1e472
Andrew Sherman <asherman@cloudera.com>
2024-03-06 15:30:38 -0800

    test_max_sessions tests webui too
    Change-Id: I84e5592ab52db9684db206001469748e693eb566

--- tests/custom_cluster/test_session_expiration.py
+++ tests/custom_cluster/test_session_expiration.py
@@ -21,6 +21,7 @@ from __future__ import absolute_import, division, print_function
 import pytest
 from impala.error import HiveServer2Error
 
+import json
 import socket
 from time import sleep
 
@@ -181,8 +182,14 @@ class TestSessionExpiration(CustomClusterTestSuite):
     client2 = impalad.service.create_hs2_client()
     client2.execute_async("select sleep(10000)")
     try:
+      # Trying to open a third session should fail.
       impalad.service.create_hs2_client()
     except Exception as e:
       assert "Number of sessions for user exceeds coordinator limit" in str(e), \
       "Unexpected exception: " + str(e)
 
+    # Test webui for hs2 sessions.
+    res = impalad.service.get_debug_webpage_json("/sessions")
+    assert res['num_sessions'] == 2
+    assert res['users'][0]['user'] is not None
+    assert res['users'][0]['session_count'] == 2

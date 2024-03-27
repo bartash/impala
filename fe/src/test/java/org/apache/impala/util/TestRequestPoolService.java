ba0c6358f09cff4dd919e07e3a425f31cce7229d
Andrew Sherman <asherman@cloudera.com>
2024-03-26 18:29:57 -0700

    IMPALA-12345: Add user quotas to Admission Control
    Allow administrators to configure per user limits on queries that can
    run in the Impala system.
    
    In order to do this, there are two parts. Firstly we must track the
    total counts of queries in the system on a per-user basis. Secondly
    there must be a user model that allows rules that control per-user
    limits on the number of queries that can be run.
    
    TPoolStats (the data that is shared between Admission Control instances)
    is extended to include a map from user name to a count of queries
    running. This (along with some derived data structures) is updated when
    queries are queued and when they are released from Admission Control.
    This lifecycle is slightly different from other TPoolStats data which
    usually tracks data about queries that are running. Queries can be
    rejected because of user quotas at submission time. This is done for
    two reasons: (1) queries can only be admitted from the front of the
    queue and we do not want to block other queries due to quotas, and
    (2) it is easy for users to understand what is going on when queries
    are rejected at submission time.
    
    Note that when running in configurations without an Admission Daemon
    then Admission Control does not have perfect information about the
    system and over-admission is possible for User-Level Admission Quotas
    in the same way that it is for other Admission Control controls.
    
    The User Model is implemented by extending the format of the
    fair-scheduler.xml file. The rules controlling the per-user limits are
    specified in terms of user or group names.
    
    Two new elements ‘userQueryLimit’ and ‘groupQueryLimit’ can be added to
    the fair-scheduler.xml file. These elements can be placed on the root
    configuration, which applies to all pools, or the pool configuration.
    The ‘userQueryLimit’ element has 2 child elements: "user" and "limit".
    The 'user' element contains the short names of users, and can be
    repeated, or have the value "*" for a wildcard name which matches all
    users. The ‘groupQueryLimit’ element has 2 child elements: "group"
    and "limit". The 'group' element contains group names.
    
    The root level rules and pool level rules must both be passed for a new
    query to be queued. The rules dictate a maximum number of queries that
    can run by a user. When evaluating rules at either the root level, or
    at the pool level, when a rule matches a user then there is no more
    evaluation done.
    
    To support reading the ‘userQueryLimit’ and ‘groupQueryLimit’ fields the
    RequestPoolService is enhanced.
    
    If user quotas are enabled for a pool then a list of the users with
    running or queued queries in that pool is visible on the coordinator
    webui admission control page.
    
    More comprehensive documentation of the user model will be provided in
    IMPALA-12943
    
    TESTING
    
    New end-to-end tests are added to test_admission_controller.py, and
    admission-controller-test is extended to provide unit tests for the
    user model.
    
    Change-Id: I4c33f3f2427db57fb9b6c593a4b22d5029549b41
    Add comment

--- fe/src/test/java/org/apache/impala/util/TestRequestPoolService.java
+++ fe/src/test/java/org/apache/impala/util/TestRequestPoolService.java
@@ -17,26 +17,14 @@
 
 package org.apache.impala.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.net.URISyntaxException;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-
 import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.HADOOP_SECURITY_AUTH_TO_LOCAL;
+import static org.apache.impala.yarn.server.resourcemanager.scheduler.fair.
+    AllocationFileLoaderService.addQueryLimits;
+
+import com.google.common.collect.Iterables;
+import com.google.common.io.Files;
 
 import org.apache.hadoop.conf.Configuration;
-
-import org.apache.impala.yarn.server.resourcemanager.scheduler.fair.AllocationFileLoaderService;
-
 import org.apache.impala.authorization.User;
 import org.apache.impala.common.ByteUnits;
 import org.apache.impala.common.InternalException;
@@ -45,9 +33,33 @@ import org.apache.impala.thrift.TErrorCode;
 import org.apache.impala.thrift.TPoolConfig;
 import org.apache.impala.thrift.TResolveRequestPoolParams;
 import org.apache.impala.thrift.TResolveRequestPoolResult;
+import org.apache.impala.yarn.server.resourcemanager.scheduler.fair.
+    AllocationConfigurationException;
+import org.apache.impala.yarn.server.resourcemanager.scheduler.fair.
+    AllocationFileLoaderService;
 import org.apache.impala.yarn.server.resourcemanager.scheduler.fair.QueuePlacementPolicy;
-import com.google.common.collect.Iterables;
-import com.google.common.io.Files;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Unit tests for the user to pool resolution, authorization, and getting configuration
@@ -63,6 +75,7 @@ public class TestRequestPoolService {
 
   // A second allocation file which overwrites the temporary file to check for changes.
   private static final String ALLOCATION_FILE_MODIFIED = "fair-scheduler-test2.xml";
+  private static final String ALLOCATION_FILE_EXTRA = "fair-scheduler-test3.xml";
   private static final String ALLOCATION_FILE_EMPTY = "fair-scheduler-empty.xml";
   private static final String ALLOCATION_FILE_GROUP_RULE = "fair-scheduler-group-rule.xml";
 
@@ -195,17 +208,40 @@ public class TestRequestPoolService {
     Assert.assertTrue(poolService_.hasAccess("root.queueB", "userB"));
     Assert.assertFalse(poolService_.hasAccess("root.queueB", "userA"));
     Assert.assertTrue(poolService_.hasAccess("root.queueB", "root"));
+    // Test that comma separated users are parsed correctly.
+    Assert.assertTrue(poolService_.hasAccess("root.queueD", "userA"));
+    Assert.assertTrue(poolService_.hasAccess("root.queueD", "userB"));
+    Assert.assertFalse(poolService_.hasAccess("root.queueD", "userZ"));
   }
 
   @Test
   public void testPoolLimitConfigs() throws Exception {
     createPoolService(ALLOCATION_FILE, LLAMA_CONFIG_FILE);
-    checkPoolConfigResult("root", 15, 50, -1, 30000L, "mem_limit=1024m");
+    Map<String, Integer> rootUserQueryLimits = new HashMap<>();
+    rootUserQueryLimits.put("userB", 6);
+    rootUserQueryLimits.put("*", 10);
+    Map<String, Integer> rootGroupQueryLimits = new HashMap<>();
+    rootGroupQueryLimits.put("group3", 5);
+    checkPoolConfigResult("root", 15, 50, -1, 30000L, "mem_limit=1024m",
+        rootUserQueryLimits, rootGroupQueryLimits);
     checkPoolConfigResult("root.queueA", 10, 30, 1024 * ByteUnits.MEGABYTE,
         10000L, "mem_limit=1024m,query_timeout_s=10");
     checkPoolConfigResult("root.queueB", 5, 10, -1, 30000L, "mem_limit=1024m");
     checkPoolConfigResult("root.queueC", 5, 10, 1024 * ByteUnits.MEGABYTE, 30000L,
-            "mem_limit=1024m", 1000, 10, false, 8, 8);
+        "mem_limit=1024m", 1000, 10, false, 8, 8, null, null);
+
+    Map<String, Integer> queueDUserQueryLimits = new HashMap<>();
+    queueDUserQueryLimits.put("userA", 2);
+    queueDUserQueryLimits.put("userF", 2);
+    queueDUserQueryLimits.put("userG", 101);
+    queueDUserQueryLimits.put("*", 3);
+    Map<String, Integer> queueDGroupQueryLimits = new HashMap<>();
+    queueDGroupQueryLimits.put("group1", 1);
+    queueDGroupQueryLimits.put("group2", 1);
+
+    checkPoolConfigResult("root.queueD", 5, 10, -1, 30000L, "mem_limit=1024m",
+        queueDUserQueryLimits, queueDGroupQueryLimits);
+
   }
 
   @Test
@@ -213,7 +249,7 @@ public class TestRequestPoolService {
     createPoolService(ALLOCATION_FILE_EMPTY, LLAMA_CONFIG_FILE_EMPTY);
     Assert.assertEquals("root.userA", poolService_.assignToPool("", "userA"));
     Assert.assertTrue(poolService_.hasAccess("root.userA", "userA"));
-    checkPoolConfigResult("root", -1, 200, -1, null, "", 0, 0, true, 0, 0);
+    checkPoolConfigResult("root", -1, 200, -1, null, "", 0, 0, true, 0, 0, null, null);
   }
 
   @Ignore("IMPALA-4868") @Test
@@ -255,6 +291,61 @@ public class TestRequestPoolService {
     checkModifiedConfigResults();
   }
 
+  /**
+   * Validate reading user and group quotas
+   */
+  @Test
+  public void testReadUserGroupQuotas() throws Exception {
+    createPoolService(ALLOCATION_FILE_EXTRA, null);
+    TPoolConfig rootConfig = poolService_.getPoolConfig("root");
+    Map<String, Integer> rootUserExpected = new HashMap<String, Integer>() {
+      {
+        put("*", 8);
+        put("howard", 4);
+      }
+    };
+    Assert.assertEquals(rootUserExpected, rootConfig.user_query_limits);
+    Map<String, Integer> rootGroupExpected = new HashMap<String, Integer>() {
+      { put("support", 6); }
+    };
+    Assert.assertEquals(rootGroupExpected, rootConfig.group_query_limits);
+
+    TPoolConfig smallConfig = poolService_.getPoolConfig("root.group-set-small");
+    Map<String, Integer> smallUserExpected = new HashMap<String, Integer>() {
+      {
+        put("*", 1);
+        put("alice", 4);
+      }
+    };
+    Assert.assertEquals(smallUserExpected, smallConfig.user_query_limits);
+    Map<String, Integer> smallGroupExpected = new HashMap<String, Integer>() {
+      {
+        put("support", 5);
+        put("dev", 5);
+        put("it", 2);
+      }
+    };
+    Assert.assertEquals(smallGroupExpected, smallConfig.group_query_limits);
+
+    TPoolConfig largeConfig = poolService_.getPoolConfig("root.group-set-large");
+    Map<String, Integer> largeUserExpected = new HashMap<String, Integer>() {
+      {
+        put("*", 1);
+        put("alice", 4);
+        put("claire", 3);
+      }
+    };
+    Assert.assertEquals(largeUserExpected, largeConfig.user_query_limits);
+    Map<String, Integer> largeGroupExpected = new HashMap<String, Integer>() {
+      {
+        put("support", 1);
+        put("dev", 2);
+      }
+    };
+    Assert.assertEquals(largeGroupExpected, largeConfig.group_query_limits);
+  }
+
+  // Test pool resolution
   @Test
   public void testNullLlamaSite() throws Exception {
     createPoolService(ALLOCATION_FILE_MODIFIED, null);
@@ -270,12 +361,162 @@ public class TestRequestPoolService {
     Assert.assertTrue(poolService_.hasAccess("root.queueB", "userA"));
     Assert.assertFalse(poolService_.hasAccess("root.queueC", "userA"));
     Assert.assertTrue(poolService_.hasAccess("root.queueC", "root"));
+    Assert.assertTrue(poolService_.hasAccess("root.queueD", "userA"));
 
     // Test pool limits
-    checkPoolConfigResult("root", -1, 200, -1);
+    Map<String, Integer> rootQueryLimits = new HashMap<>();
+    Map<String, Integer> rootGroupLimits = new HashMap<>();
+    rootQueryLimits.put("userD", 2);
+    checkPoolConfigResult(
+        "root", -1, 200, -1, null, "", rootQueryLimits, rootGroupLimits);
     checkPoolConfigResult("root.queueA", -1, 200, 100000 * ByteUnits.MEGABYTE);
     checkPoolConfigResult("root.queueB", -1, 200, -1);
     checkPoolConfigResult("root.queueC", -1, 200, 128 * ByteUnits.MEGABYTE);
+
+    Map<String, Integer> queueEUserQueryLimits = new HashMap<>();
+    queueEUserQueryLimits.put("userA", 3);
+    queueEUserQueryLimits.put("userG", 3);
+    queueEUserQueryLimits.put("userH", 0);
+    queueEUserQueryLimits.put("*", 1);
+    Map<String, Integer> queueEGroupQueryLimits = new HashMap<>();
+    queueEGroupQueryLimits.put("group1", 2);
+    queueEGroupQueryLimits.put("group2", 2);
+
+    checkPoolConfigResult("root.queueE", -1, 200, -1, null, "",
+        queueEUserQueryLimits, queueEGroupQueryLimits);
+  }
+
+  /**
+   * Parse a snippet of xml, and then call addQueryLimits() on the root element.
+   */
+  private static Map<String, Integer> doQueryLimitParsing(String xmlString, String name)
+      throws ParserConfigurationException, SAXException, IOException,
+             AllocationConfigurationException {
+    // Create a DocumentBuilderFactory instance
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    // Create a DocumentBuilder instance
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    // Parse the XML string into a Document object
+    Document document =
+        builder.parse(new java.io.ByteArrayInputStream(xmlString.getBytes()));
+    // Get the root element
+    Element rootElement = document.getDocumentElement();
+
+    Map<String, Map<String, Integer>> userQueryLimits = new HashMap<>();
+    String queueName = "queue1";
+    addQueryLimits(queueName, rootElement, "userQueryLimit", userQueryLimits, name);
+    return userQueryLimits.get(queueName);
+  }
+
+  /**
+   * Call doQueryLimitParsing() and check that it threw an exception, and that the
+   * exception message contains the expected text.
+   */
+  private static void assertFailureMessage(String xmlString, String expectedError) {
+    try {
+      doQueryLimitParsing(xmlString, "user");
+      Assert.fail(
+          "did not get expected exception, with expected message " + expectedError);
+    } catch (Exception e) {
+      Assert.assertTrue(e instanceof AllocationConfigurationException);
+
+      Assert.assertTrue(e.getMessage().contains(expectedError));
+    }
+  }
+
+  /**
+   * Unit test for doQueryLimitParsing().
+   */
+  @Test
+  public void testLimitsParsing() throws Exception {
+    String xmlString = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<userQueryLimit>",
+        "    <user>John</user>",
+        "    <totalCount>30</totalCount>",
+        "</userQueryLimit>"
+    );
+    Map<String, Integer> expected = new HashMap<String, Integer>() {{
+      put("John", 30);
+    }};
+
+    Map<String, Integer> parsed = doQueryLimitParsing(xmlString, "user");
+    Assert.assertEquals(expected, parsed);
+
+    String xmlString2 = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<userQueryLimit>",
+        "    <user>John</user>",
+        "    <user>Barry</user>",
+        "    <totalCount>30</totalCount>",
+        "</userQueryLimit>"
+    );
+    Map<String, Integer> expected2 = new HashMap<String, Integer>() {{
+      put("John", 30);
+      put("Barry", 30);
+    }};
+
+    Map<String, Integer> parsed2 = doQueryLimitParsing(xmlString2, "user");
+    Assert.assertEquals(expected2, parsed2);
+
+    String xmlString3 = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<groupQueryLimit>",
+        "    <group>group1</group>",
+        "    <group>group2</group>",
+        "    <totalCount>1</totalCount>",
+        "</groupQueryLimit>"
+    );
+    Map<String, Integer> expected3 = new HashMap<String, Integer>() {{
+      put("group1", 1);
+      put("group2", 1);
+    }};
+    Map<String, Integer> parsed3 = doQueryLimitParsing(xmlString3, "group");
+    Assert.assertEquals(expected3, parsed3);
+  }
+
+
+  /**
+   * Unit test for doQueryLimitParsing() error cases.
+   */
+  @Test
+  public void testLimitsParsingErrors() throws Exception {
+    String xmlString1 = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<userQueryLimit>",
+        "    <totalCount>30</totalCount>",
+        "</userQueryLimit>"
+    );
+    assertFailureMessage(xmlString1, "Empty user names");
+    String xmlString2 = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<userQueryLimit>",
+        "    <user>John</user>",
+        "    <user>Barry</user>",
+        "    <totalCount>30</totalCount>",
+        "    <totalCount>31</totalCount>",
+        "</userQueryLimit>"
+    );
+    assertFailureMessage(xmlString2, "Duplicate totalCount tags");
+    String xmlString3 = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<userQueryLimit>",
+        "    <user>John</user>",
+        "    <user>Barry</user>",
+        "    <totalCount>fish</totalCount>",
+        "</userQueryLimit>"
+    );
+    assertFailureMessage(xmlString3, "Could not parse query totalCount");
+    String xmlString4 = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<userQueryLimit>",
+        "    <user>John</user>",
+        "    <user>Barry</user>",
+        "</userQueryLimit>"
+    );
+    assertFailureMessage(xmlString4, "No totalCount for");
+
+    String xmlString5 = String.join("\n", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+        "<userQueryLimit>",
+        "    <user>John</user>",
+        "    <user>John</user>",
+        "    <totalCount>30</totalCount>",
+        "</userQueryLimit>"
+    );
+    assertFailureMessage(xmlString5, "Duplicate value given for name");
   }
 
   private void checkModifiedConfigResults()
@@ -293,14 +534,18 @@ public class TestRequestPoolService {
     Assert.assertTrue(poolService_.hasAccess("root.queueC", "root"));
 
     // Test pool limit changes
-    checkPoolConfigResult("root", 15, 100, -1, 30000L, "");
+    Map<String, Integer> rootQueryLimits = new HashMap<>();
+    Map<String, Integer> rootGroupLimits = new HashMap<>();
+    rootQueryLimits.put("userD", 2);
+    checkPoolConfigResult(
+        "root", 15, 100, -1, 30000L, "", rootQueryLimits, rootGroupLimits);
     // not_a_valid_option=foo.bar gets filtered out when parsing the query options on
     // the backend, but it should be observed coming from the test file here.
-    checkPoolConfigResult("root.queueA", 1, 30, 100000 * ByteUnits.MEGABYTE,
-        50L, "mem_limit=128m,query_timeout_s=5,not_a_valid_option=foo.bar");
+    checkPoolConfigResult("root.queueA", 1, 30, 100000 * ByteUnits.MEGABYTE, 50L,
+        "mem_limit=128m,query_timeout_s=5,not_a_valid_option=foo.bar");
     checkPoolConfigResult("root.queueB", 5, 10, -1, 600000L, "");
-    checkPoolConfigResult("root.queueC", 10, 30, 128 * ByteUnits.MEGABYTE,
-        30000L, "mem_limit=2048m,query_timeout_s=60");
+    checkPoolConfigResult("root.queueC", 10, 30, 128 * ByteUnits.MEGABYTE, 30000L,
+        "mem_limit=2048m,query_timeout_s=60");
   }
 
   /**
@@ -310,7 +555,8 @@ public class TestRequestPoolService {
       long expectedMaxQueued, long expectedMaxMem, Long expectedQueueTimeoutMs,
       String expectedQueryOptions, long max_query_mem_limit, long min_query_mem_limit,
       boolean clamp_mem_limit_query_option, long max_query_cpu_core_per_node_limit,
-      long max_query_cpu_core_coordinator_limit) {
+      long max_query_cpu_core_coordinator_limit, Map<String, Integer> userQueryLimits,
+      Map<String, Integer> groupQueryLimits) {
     TPoolConfig expectedResult = new TPoolConfig();
     expectedResult.setMax_requests(expectedMaxRequests);
     expectedResult.setMax_queued(expectedMaxQueued);
@@ -328,15 +574,30 @@ public class TestRequestPoolService {
     if (expectedQueryOptions != null) {
       expectedResult.setDefault_query_options(expectedQueryOptions);
     }
-    Assert.assertEquals("Unexpected config values for pool " + pool,
-        expectedResult, poolService_.getPoolConfig(pool));
+    expectedResult.setUser_query_limits(
+        userQueryLimits != null ? userQueryLimits : Collections.emptyMap());
+    expectedResult.setGroup_query_limits(
+        groupQueryLimits != null ? groupQueryLimits : Collections.emptyMap());
+    TPoolConfig poolConfig = poolService_.getPoolConfig(pool);
+    Assert.assertEquals(
+        "Unexpected config values for pool " + pool, expectedResult, poolConfig);
   }
 
   private void checkPoolConfigResult(String pool, long expectedMaxRequests,
       long expectedMaxQueued, long expectedMaxMem, Long expectedQueueTimeoutMs,
       String expectedQueryOptions) {
+    checkPoolConfigResult( pool,  expectedMaxRequests,
+     expectedMaxQueued,  expectedMaxMem,  expectedQueueTimeoutMs,
+         expectedQueryOptions, Collections.emptyMap(), Collections.emptyMap());
+  }
+
+  private void checkPoolConfigResult(String pool, long expectedMaxRequests,
+      long expectedMaxQueued, long expectedMaxMem, Long expectedQueueTimeoutMs,
+      String expectedQueryOptions, Map<String, Integer> userQueryLimits,
+      Map<String, Integer> groupQueryLimits) {
     checkPoolConfigResult(pool, expectedMaxRequests, expectedMaxQueued, expectedMaxMem,
-        expectedQueueTimeoutMs, expectedQueryOptions, 0, 0, true, 0, 0);
+        expectedQueueTimeoutMs, expectedQueryOptions, 0, 0, true, 0, 0,
+        userQueryLimits, groupQueryLimits);
   }
 
   private void checkPoolConfigResult(String pool, long expectedMaxRequests,

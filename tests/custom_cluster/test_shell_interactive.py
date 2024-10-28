cfd3fc66fb97afb439fce8b2688229b099a040a6
Andrew Sherman <asherman@cloudera.com>
2024-10-28 12:26:56 -0700

    set header coreectly
    Change-Id: I917f01d5e072351926ceb245ddeea09126b11a7b

--- tests/custom_cluster/test_shell_interactive.py
+++ tests/custom_cluster/test_shell_interactive.py
@@ -52,7 +52,7 @@ class RequestHandlerProxy(http.server.SimpleHTTPRequestHandler):
 
   def do_POST(self):
     data_string = self.rfile.read(int(self.headers['Content-Length']))
-    self.headers.addheader('X-Forwarded-For', "127.0.0.1")
+    self.headers['X-Forwarded-For'] = "127.0.0.1"
     response = requests.post(url="http://localhost:28000/cliservice", headers=self.headers, data=data_string)
     self.send_response(code=response.status_code)
     for key, value in response.headers.iteritems():
@@ -221,7 +221,7 @@ class TestShellInteractive(CustomClusterTestSuite):
                     '--connect_max_tries=1']
     proc = spawn_shell(get_shell_cmd(vector, host_port="localhost:{0}".format(http_proxy_server.PORT)) + shell_params)
     # Check that we connect OK
-    proc.expect(pattern="{0}] default>".format(get_impalad_port(vector)), timeout=10)
+    proc.expect(pattern="{0}] default>".format(get_impalad_port(vector)), timeout=20)
 
 
   def __proc_not_expect(self, proc, pattern):

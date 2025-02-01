ab2c6b3fba4f7073d456ac6c4981abff9ecc16b3
Andrew Sherman <asherman@cloudera.com>
2025-02-01 11:41:46 -0800

    add assertion  2
    Change-Id: If788bc3ce1b665ee7ae627570e5bd3c5c6e29a01

--- be/src/service/query-state-record-test.cc
+++ be/src/service/query-state-record-test.cc
@@ -156,15 +156,20 @@ TEST(QueryStateRecordTest, PerHostStatePeakMemoryComparatorEqual) {
 
 TEST(QueryStateRecordTest, AdmissionSlots) {
   QuerySchedulePB query_schedule;
-  BackendExecParamsPB* params = query_schedule.add_backend_exec_params();
+  BackendExecParamsPB* params;
+  params = query_schedule.add_backend_exec_params();
   params->set_is_coord_backend(true);
   params->set_slots_to_use(4);
+  params = query_schedule.add_backend_exec_params();
+  params->set_slots_to_use(7);
+  params = query_schedule.add_backend_exec_params();
+  params->set_slots_to_use(7;
   int coordinator_slots = QueryStateRecord::get_coordinator_slots(&query_schedule);
   int executor_slots = QueryStateRecord::get_executor_slots(&query_schedule);
   std::cout << " coordinator_slots=" << coordinator_slots << std::endl;
   std::cout << " executor_slots=" << executor_slots << std::endl;
   EXPECT_EQ(coordinator_slots, 4);
-  EXPECT_EQ(executor_slots, 0);
+  EXPECT_EQ(executor_slots, 7);
 }
 
 } //namespace impala

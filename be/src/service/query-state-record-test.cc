51a90315839345252f9e5a9ee0f171383222c4e9
Andrew Sherman <asherman@cloudera.com>
2025-02-01 11:22:25 -0800

    allocate on stack again
    Change-Id: I8ed1247514eeea6af569f928e1040621ba720346

--- be/src/service/query-state-record-test.cc
+++ be/src/service/query-state-record-test.cc
@@ -155,9 +155,10 @@ TEST(QueryStateRecordTest, PerHostStatePeakMemoryComparatorEqual) {
 }
 
 TEST(QueryStateRecordTest, AdmissionSlots) {
-  QuerySchedulePB *query_schedule = new QuerySchedulePB();
-  int coordinator_slots = QueryStateRecord::get_coordinator_slots(query_schedule);
-  int executor_slots = QueryStateRecord::get_executor_slots(query_schedule);
+  QuerySchedulePB query_schedule;
+
+  int coordinator_slots = QueryStateRecord::get_coordinator_slots(&query_schedule);
+  int executor_slots = QueryStateRecord::get_executor_slots(&query_schedule);
   std::cout << " coordinator_slots=" << coordinator_slots << std::endl;
   std::cout << " executor_slots=" << executor_slots << std::endl;
 }

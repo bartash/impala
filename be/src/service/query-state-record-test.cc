bf8b8ee7a99aa3e8e0a26f57ee9e2e3875edbc0b
Andrew Sherman <asherman@cloudera.com>
2025-02-01 11:42:11 -0800

    add assertion  2
    Change-Id: I5d2212b0b90e9a4e2876251cb072f16e024d2162

--- be/src/service/query-state-record-test.cc
+++ be/src/service/query-state-record-test.cc
@@ -163,7 +163,7 @@ TEST(QueryStateRecordTest, AdmissionSlots) {
   params = query_schedule.add_backend_exec_params();
   params->set_slots_to_use(7);
   params = query_schedule.add_backend_exec_params();
-  params->set_slots_to_use(7;
+  params->set_slots_to_use(7);
   int coordinator_slots = QueryStateRecord::get_coordinator_slots(&query_schedule);
   int executor_slots = QueryStateRecord::get_executor_slots(&query_schedule);
   std::cout << " coordinator_slots=" << coordinator_slots << std::endl;

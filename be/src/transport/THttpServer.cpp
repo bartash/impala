9a89d5d53386e4741a1c712361eb2c31f7e0a479
Andrew Sherman <asherman@cloudera.com>
2025-02-05 16:21:25 -0800

    Revert "add logging"
    This reverts commit 1ad216862b35b70f47c94be4e1aa7e34eadf6139.

--- be/src/transport/THttpServer.cpp
+++ be/src/transport/THttpServer.cpp
@@ -176,11 +176,7 @@ void THttpServer::parseHeader(char* header) {
   } else if (MatchesHeader(header, HEADER_X_FORWARDED_FOR, sz)) {
     // Only set the origin field the first time that we see the 'X-Forwarded-For' header.
     if (origin_.empty()) {
-      VLOG_QUERY << "orign empty set XFF=" << value;
       origin_ = value;
-    }
-    else {
-      VLOG_QUERY << "orign already has " << origin_ << " so not setting XFF=" << value;
     }
   } else if ((has_ldap_ || has_kerberos_ || has_saml_ || has_jwt_ || has_oauth_)
       && MatchesHeader(header, HEADER_AUTHORIZATION, sz)) {
